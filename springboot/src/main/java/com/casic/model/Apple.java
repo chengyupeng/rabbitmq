aac41172cd67b5e644d1be728ce963c4621528c8
chengyupeng <2416925747@qq.com>
2018-12-14 15:55:18 +0800

    send commit

---
+++ springboot/src/main/java/com/casic/model/Apple.java
@@ -0,0 +1,17 @@
+package com.casic.model;
+
+public class Apple {
+	
+	private   String name="apple";
+
+	public  String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+   
+}
---
+++ springboot/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@page import="com.casic.model.Apple"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<html>
+
+<body>
+<h2> <%=new Apple().getName() %></h2>
+</body>
+</html>
